b887fe665c5843ce492a5a0c80a7fdbaf8da2a34
soredake <fdsfgs@krutt.org>
2018-07-15 12:44:55 +0300

    commit

--- truckers-mp.sh
+++ truckers-mp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-listurl=https://update.ets2mp.com
+listurl=http://update.ets2mp.com
 dlurl=http://download.ets2mp.com/files
 base="$PWD"
 dldir="$base/prefix/drive_c/truckersmp"
 
 mkdir -p "$dldir"
-wget "$listurl/files.json" -O "./files.json"
+wget --no-check-certificate "$listurl/files.json" -O "./files.json"
 
 cat "./files.json" | awk 'BEGIN{i=0}/":"/{printf $0;i=1;next}{if (i) {printf "\n"; i=0}}' | awk -F '"' '{printf "%s %s\n",$8,$4}' > "./.checksums"
 files=( $(cat "./.checksums" | md5sum -c --quiet - 2> /dev/null | awk -F ':' '{print $1}') )
@@ -14,7 +14,7 @@ files=( $(cat "./.checksums" | md5sum -c --quiet - 2> /dev/null | awk -F ':' '{p
 for f in ${files[@]}; do
     echo "f = $f"
     mkdir -p "$dldir/$(dirname $f)"
-    wget $dlurl/$f -O "$dldir/$f"
+    wget --no-check-certificate $dlurl/$f -O "$dldir/$f"
 done
 
 rm -f ".checksums" "files.json"
